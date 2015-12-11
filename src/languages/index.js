e04bf4c96483aaec234e9f2e144884090fd3fe16
Vivek Kumar Bansal <vkb0310@gmail.com>
2015-12-11 18:24:04 +0530

    Add JSX

--- src/languages/index.js
+++ src/languages/index.js
@@ -2,5 +2,6 @@
 
 export * from "./clike";
 export * from "./javascript";
+// export * from "./jsx";
 export * from "./markup";
 export * from "./php";
---
+++ src/languages/jsx.js
@@ -0,0 +1,27 @@
+"use strict";
+
+import { lang } from "../utils";
+import { javascript } from "./javascript";
+import { markup } from "./markup";
+
+let jsx = lang.extend(markup, javascript);
+
+jsx.tag.pattern = /<\/?[\w:-]+\s*(?:\s+[\w:-]+(?:=(?:("|')(\\?[\w\W])*?\1|[^\s'">=]+|(\{[\w\W]*?\})))?\s*)*\/?>/i;
+jsx.tag.inside["attr-value"].pattern = /=[^\{](?:('|")[\w\W]*?(\1)|[^\s>]+)/i;
+
+lang.insertBefore(jsx.tag.inside, "attr-value", {
+	"script": {
+		// Allow for one level of nesting
+		pattern: /=(\{(?:\{[^}]*\}|[^}])+\})/i,
+		inside:{
+            'function': javascript.function,
+            'punctuation': /[={}[\];(),.:]/,
+            'keyword': javascript.keyword,
+            'boolean': javascript.boolean
+        },
+		'alias': 'language-javascript'
+	}
+});
+
+export { jsx as jsx };
+export { jsx as babel };
