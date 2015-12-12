eb7e597984e2c0799d18d04cfa6aca2c89af1674
Vivek Kumar Bansal <vkb0310@gmail.com>
2015-12-12 14:17:44 +0530

    Fix and include JSX

--- src/languages/jsx.js
+++ src/languages/jsx.js
@@ -10,17 +10,19 @@ jsx.tag.pattern = /<\/?[\w:-]+\s*(?:\s+[\w:-]+(?:=(?:("|')(\\?[\w\W])*?\1|[^\s'"
 jsx.tag.inside["attr-value"].pattern = /=[^\{](?:('|")[\w\W]*?(\1)|[^\s>]+)/i;
 
 lang.insertBefore(jsx.tag.inside, "attr-value", {
-	"script": {
-		// Allow for one level of nesting
-		pattern: /=(\{(?:\{[^}]*\}|[^}])+\})/i,
-		inside:{
-            'function': javascript.function,
-            'punctuation': /[={}[\];(),.:]/,
-            'keyword': javascript.keyword,
-            'boolean': javascript.boolean
+    "script": {
+        // Allow for one level of nesting
+        pattern: /=(\{(?:\{[^}]*\}|[^}])+\})/i,
+        inside: {
+            "function": javascript.function,
+            punctuation: /[={}[\];(),.:]/,
+            keyword: javascript.keyword,
+            "boolean": javascript.boolean,
+            _order: ["function", "punctuation", "keyword", "boolean"]
         },
-		'alias': 'language-javascript'
-	}
+        "alias": "language-javascript"
+    },
+    _order: ["script"]
 });
 
 export { jsx as jsx };
