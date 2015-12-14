37efdefc1d340a72c1e0e693b6e573e72a0d9b6b
Vivek Kumar Bansal <vkb0310@gmail.com>
2015-12-14 19:13:05 +0530

    Added LESS

--- src/languages/index.js
+++ src/languages/index.js
@@ -4,5 +4,6 @@ export * from "./clike";
 export * from "./css";
 export * from "./javascript";
 export * from "./jsx";
+export * from "./less";
 export * from "./markup";
 export * from "./php";
---
+++ src/languages/less.js
@@ -0,0 +1,73 @@
+"use strict";
+
+import { lang } from "../utils";
+import { css } from "./css";
+
+/* FIXME :
+ :extend() is not handled specifically : its highlighting is buggy.
+ Mixin usage must be inside a ruleset to be highlighted.
+ At-rules (e.g. import) containing interpolations are buggy.
+ Detached rulesets are highlighted as at-rules.
+ A comment before a mixin usage prevents the latter to be properly highlighted.
+ */
+
+let less = lang.extend(css, {
+    comment: [
+        /\/\*[\w\W]*?\*\//,
+        {
+            pattern: /(^|[^\\])\/\/.*/,
+            lookbehind: true
+        }
+    ],
+    atrule: {
+        pattern: /@[\w-]+?(?:\([^{}]+\)|[^(){};])*?(?=\s*\{)/i,
+        inside: {
+            punctuation: /[:()]/,
+            _order: ["inside"]
+        }
+    },
+    // selectors and mixins are considered the same
+    selector: {
+        pattern: /(?:@\{[\w-]+\}|[^{};\s@])(?:@\{[\w-]+\}|\([^{}]*\)|[^{};@])*?(?=\s*\{)/,
+        inside: {
+            // mixin parameters
+            variable: /@+[\w-]+/,
+            _order: ["selector"]
+        }
+    },
+
+    property: /(?:@\{[\w-]+\}|[\w-])+(?:\+_?)?(?=\s*:)/i,
+    punctuation: /[{}();:,]/,
+    operator: /[+\-*\/]/,
+    _order: ["comment", "atrule", "selector", "property", "punctuation", "operator"]
+});
+
+// Invert function and punctuation positions
+lang.insertBefore(less, "punctuation", {
+    "function": less.function,
+    _order: ["function"]
+});
+
+lang.insertBefore(less, "property", {
+    variable: [
+        // Variable declaration (the colon must be consumed!)
+        {
+            pattern: /@[\w-]+\s*:/,
+            inside: {
+                punctuation: /:/,
+                _order: ["punctuation"]
+            }
+        },
+
+        // Variable usage
+        /@@?[\w-]+/
+    ],
+    "mixin-usage": {
+        pattern: /([{;]\s*)[.#](?!\d)[\w-]+.*?(?=[(;])/,
+        lookbehind: true,
+        alias: "function"
+    },
+    _order: ["variable", "mixin-usage"]
+});
+
+export { less as less };
