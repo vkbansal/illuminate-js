7c208ce9062b07b87c2f7c445380884e02bde659
Vivek Kumar Bansal <vivek.bansal@flipkart.com>
2017-02-01 23:29:55 +0530

    add utils with test

---
+++ illuminate/src/utils.js
@@ -0,0 +1,14 @@
+import Token from './Token';
+
+export function encode(tokens) {
+    if (tokens instanceof Token) {
+        return new Token(tokens.type, encode(tokens.content), tokens.alias);
+    } else if (Array.isArray(tokens)) {
+        return tokens.map(encode);
+    }
+    return tokens
+            .replace(/&/g, '&amp;')
+            .replace(/</g, '&lt;')
+            .replace(/>/g, '&gt;')
+            .replace(/\u00a0/g, ' ');
+}
