9170133001385500ae6e91f3ce41fcf5d2c064b1
Vivek Kumar Bansal <vkb0310@gmail.com>
2017-11-27 17:14:46 +0530

    update hooks

--- packages/illuminate/src/hooks.ts
+++ packages/illuminate/src/hooks.ts
@@ -1,16 +1,22 @@
-interface Hooks {
-    [key: string]: Set<(...args: any[]) => void>
-}
+import { HighlightEnv } from './illuminate';
+import { TokenEnv } from './Token';
 
-const hooks: Hooks = {};
+const hooks: Record<string, Set<(...args: any[]) => void>> = {};
 
-export function add(name: string, callback: (...args: any[]) => void) {
+export function add(
+    name: 'after-highlight' | 'before-highlight' | 'wrap',
+    callback: (env: HighlightEnv | TokenEnv) => void
+): void {
     if (typeof name !== 'string' || name.length < 1) {
         throw new Error('Name must be string of length > 1');
     }
 
     if (typeof callback !== 'function') {
-        throw new Error(`hooks.add expects a function to be passed as a callback but ${typeof callback}:${callback} given`);
+        throw new Error(
+            `hooks.add expects a function to be passed as a callback but ${typeof callback}:${
+                callback
+            } given`
+        );
     }
 
     if (!hooks[name]) hooks[name] = new Set();
@@ -18,14 +24,12 @@ export function add(name: string, callback: (...args: any[]) => void) {
     hooks[name].add(callback);
 }
 
-export function run(name: string, env: object) {
+export function run(name: string, env: object): void {
     const callbacks = hooks[name];
 
     if (!callbacks || !callbacks.size) {
         return;
     }
 
-    callbacks.forEach((callback) => callback(env));
+    callbacks.forEach(callback => callback(env));
 }
-
-export default { add, run };
