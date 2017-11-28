0d690b4926dcc66d35706bbeb9ef34e154103ded
Vivek Kumar Bansal <vkb0310@gmail.com>
2017-11-28 15:41:30 +0530

    update languages

---
+++ packages/illuminate/src/languages/clike.ts
@@ -0,0 +1,41 @@
+import { Definition, TokenTypes } from '../illuminate';
+
+export const clike: Definition = new Map<string, TokenTypes>([
+    [
+        'comment',
+        [
+            {
+                pattern: /(^|[^\\])\/\*[\w\W]*?\*\//,
+                lookbehind: true
+            },
+            {
+                pattern: /(^|[^\\:])\/\/.*/,
+                lookbehind: true
+            }
+        ]
+    ],
+    [
+        'string',
+        {
+            pattern: /(["'])(\\(?:\r\n|[\s\S])|(?!\1)[^\\\r\n])*\1/,
+            greedy: true
+        }
+    ],
+    [
+        'class-name',
+        {
+            pattern: /((?:\b(?:class|interface|extends|implements|trait|instanceof|new)\s+)|(?:catch\s+\())[a-z0-9_\.\\]+/i,
+            lookbehind: true,
+            inside: new Map([['punctuation', /(\.|\\)/]])
+        }
+    ],
+    [
+        'keyword',
+        /\b(if|else|while|do|for|return|in|instanceof|function|new|try|throw|catch|finally|null|break|continue)\b/
+    ],
+    ['boolean', /\b(true|false)\b/],
+    ['function', /[a-z0-9_]+(?=\()/i],
+    ['number', /\b-?(?:0x[\da-f]+|\d*\.?\d+(?:e[+-]?\d+)?)\b/i],
+    ['operator', /--?|\+\+?|!=?=?|<=?|>=?|==?=?|&&?|\|\|?|\?|\*|\/|~|\^|%/],
+    ['punctuation', /[{}[\];(),.:]/]
+]);
--- packages/illuminate/src/languages/matlab.ts
+++ packages/illuminate/src/languages/matlab.ts
@@ -1,15 +1,15 @@
-import Definition from '../Definition';
+import { Definition, TokenTypes } from '../illuminate';
 
-export default new Definition([
+export const matlab: Definition = new Map<string, TokenTypes>([
     // We put string before comment, because of printf() patterns that contain "%"
     ['string', /\B'(?:''|[^'\n])*'/],
-    ['comment', [
-        /%\{[\s\S]*?\}%/,
-        /%.+/
-    ]],
+    ['comment', [/%\{[\s\S]*?\}%/, /%.+/]],
     // FIXME We could handle imaginary numbers as a whole
     ['number', /\b-?(?:\d*\.?\d+(?:[eE][+-]?\d+)?(?:[ij])?|[ij])\b/],
-    ['keyword', /\b(?:break|case|catch|continue|else|elseif|end|for|function|if|inf|NaN|otherwise|parfor|pause|pi|return|switch|try|while)\b/],
+    [
+        'keyword',
+        /\b(?:break|case|catch|continue|else|elseif|end|for|function|if|inf|NaN|otherwise|parfor|pause|pi|return|switch|try|while)\b/
+    ],
     ['function', /(?!\d)\w+(?=\s*\()/],
     ['operator', /\.?[*^\/\\']|[+\-:@]|[<>=~]=?|&&?|\|\|?/],
     ['punctuation', /\.{3}|[.,;\[\](){}!]/]
