32e8d9de25d5d06a0182a2efc43eecc69e96f316
Vivek Kumar Bansal <vkb0310@gmail.com>
2017-12-25 21:10:29 +0530

    add lineNumber and showLanguage features in react

--- packages/react-illuminate/src/Illuminate.tsx
+++ packages/react-illuminate/src/Illuminate.tsx
@@ -6,11 +6,25 @@ import { TokenElement, Token, SingleOrArray } from './Token';
 export interface IlluminateProps {
     children: string;
     lang?: string;
+    lineNumbers?: boolean;
+    showLanguage?: boolean;
 }
 
 export class Illuminate extends React.Component<IlluminateProps> {
     shouldComponentUpdate(nextProps: IlluminateProps) {
         return this.props.children !== nextProps.children || this.props.lang !== nextProps.lang;
+    }
+
+    renderLineNumbers(code: string) {
+        const match = code.match(/\n(?!$)/g);
+        const linesNum = match ? match.length + 1 : 1;
+        let lines = [];
+
+        for (let i = 0; i < linesNum; i++) {
+            lines.push(<span key={`line-${i}`} className="line-number" />);
+        }
+
+        return <span className="line-number-rows">{lines}</span>;
     }
 
     render() {
@@ -28,7 +42,9 @@ export class Illuminate extends React.Component<IlluminateProps> {
         return (
             <pre className={`language-${lang}`}>
                 <code>
+                    {this.props.showLanguage && <span className="show-language">{lang}</span>}
                     <TokenElement token={token} />
+                    {this.props.lineNumbers && this.renderLineNumbers(children)}
                 </code>
             </pre>
         );
